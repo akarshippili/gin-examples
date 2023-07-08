107692cfa14c77cf9f8f3557669087d376c2e13a
IPPILIAKARSH <ippiliakarsh@gmail.com>
2023-07-08 15:48:58 +0530

    added middleware

--- main.go
+++ main.go
@@ -2,12 +2,27 @@ package main
 
 import (
 	"log"
+	"net/url"
 
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	r := gin.Default()
+
+	// adding a middleware
+	r.Use(func(ctx *gin.Context) {
+		log.Println("Params")
+
+		request := ctx.Request
+		values, _ := url.ParseQuery(request.URL.RawQuery)
+
+		for key, value := range values {
+			log.Printf("Key: %v, Value: %v", key, value)
+		}
+		ctx.Next()
+	})
+
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "pong",
